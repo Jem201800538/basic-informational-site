cd45992fafa1065b24aba881f1672e981a8ea058
Jem201800538 <mo201800538@wmsu.edu.ph>
2025-07-07 10:44:28 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+import {
+    createServer
+} from "node:http";
+import fs from "fs/promises";
+import url from "url";
+import path from "path";
+
+// Get current path
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const server = createServer(async (req, res) => {
+    try {
+        if (req.method === "GET") {
+            let filepath;
+            if (req.url === "/") {
+                filepath = path.join(__dirname, 'templates', 'index.html');
+            } else if (req.url === "/about") {
+                filepath = path.join(__dirname, 'templates', 'about.html');
+            } else if (req.url === "/contact-me") {
+                filepath = path.join(__dirname, 'templates', 'contact-me.html');
+            } else {
+                filepath = path.join(__dirname, 'templates', '404.html');
+            }
+
+            const data = await fs.readFile(filepath);
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
+            res.end(data);
+        } else {
+            throw new Error("Method not allowed!")
+        }
+
+    } catch (error) {
+        res.writeHead(500, {
+            'Content-Type': 'text/plain'
+        });
+        res.end('Server Error');
+    }
+});
+
+server.listen(8080, () => {
+    console.log('Server running at http://localhost:8080/')
+});
